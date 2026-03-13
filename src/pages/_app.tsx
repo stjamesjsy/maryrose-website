cac679e230d4aab89681cdea80e7475942b50d74
Luke <luke.wyatt21@gmail.com>
2026-03-13 17:29:11 +0000

    Fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,8 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
-
+                
                 <title>The Mary Rose</title>
             </Head>
             <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ import Script from "next/script";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
